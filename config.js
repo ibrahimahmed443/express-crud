1434386377e7ba3d08e50d261ba27062a0ddd6d3
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-09-03 19:17:21 +0500

    using actual credentials of mongolab

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var config = {};
 config.options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 
-config.dbUser = "<DB_USER_HERE>";
-config.dbPassword = "<DB_PASSWORD_HERE>";
+config.dbUser = "admin";
+config.dbPassword = "admin";
 
 module.exports = config;    
 
